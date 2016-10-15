937aad7756095e5566fcbed3acd831946312b270
Punk <jump.e.r@yandex.ru>
2016-10-16 02:42:20 +0300

    migrate photo

--- database/migrations/2016_10_15_233829_photo.php
+++ database/migrations/2016_10_15_233829_photo.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Photo extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('domain', 64);
+            $table->string('path', 256);
+            $table->string('name_hash', 64);
+            $table->string('extension', 5);
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('photos');
     }
 }
