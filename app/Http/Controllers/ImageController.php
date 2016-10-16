b9982f60db1c735939dcb53bb6ae6e0ae02f19a4
Punk <jump.e.r@yandex.ru>
2016-10-16 03:53:35 +0300

    view

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,7 +7,12 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-class Controller extends BaseController
+class ImageController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        return view('image');
+    }
 }
